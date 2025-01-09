7c2069bf5cae6744cf2a9d8f31c833dbcb9f6eac
tamas <tomi20v@gmail.com>
2025-01-09 01:10:39 +0100

    MGS-31-Generator-generates-on-click - abstracted randomArrayItem

--- src/lib/game/piece/PieceFactory.ts
+++ src/lib/game/piece/PieceFactory.ts
@@ -1,14 +1,13 @@
 import Piece from "$lib/game/piece/Piece";
 import pieceCatalogue from "$lib/game/piece/pieceCatalogue";
 import type IProtoPiece from "$lib/game/piece/IProtoPiece";
-
-const getRandomElement = (arr: any[]) => arr[Math.floor(Math.random() * arr.length)];
+import {randomArrayItem} from "$lib/util";
 
 export default class PieceFactory {
 
   // @todo create one random piece. At some point we'd need parameters (which pieces are available)
   randomPiece(): Piece {
-    const proto: IProtoPiece = getRandomElement(pieceCatalogue);
+    const proto: IProtoPiece = randomArrayItem(pieceCatalogue);
     const piece = new Piece(
       proto.color,
       proto.pixelMap
