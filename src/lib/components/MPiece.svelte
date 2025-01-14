c63b77ceb2606308322afd2434c6bd14f225ec93
tamas <tomi20v@gmail.com>
2025-01-14 23:25:01 +0100

    MGS-31-Generator-generates-on-click - improved piece size

--- src/lib/components/MPiece.svelte
+++ src/lib/components/MPiece.svelte
@@ -1,6 +1,6 @@
-<div class="flex grow flex-col gap-0">
+<div class="flex grow flex-col gap-px" style:max-width="{maxWidth}">
     {#each piece.pixelMap as eachRow}
-        <div class="flex grow flex-row gap-0">
+        <div class="flex grow flex-row gap-px">
             {#each eachRow as eachCell}
                 <div class="flex aspect-square grow {eachCell == 1 ? color: 'bg-transparent'}
                             border border-solid {eachCell ? 'border-slate-700' : 'border-transparent'}"
@@ -13,5 +13,13 @@
 
   let { piece } = $props();
   let color = $derived('bg-' + piece.color);
+  let maxWidth = $derived.by(() => {
+    const h = piece.pixelMap.length;
+    const w = piece.pixelMap[0].length;
+    if ((h >2) || (w <= 2)) {
+      return "66%";
+    }
+    return null;
+  });
 
 </script>
