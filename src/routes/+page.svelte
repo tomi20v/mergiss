2002e4716766645cf765bcb2ba8de7cda9962d59
tamas <tomi20v@gmail.com>
2025-01-16 00:47:15 +0100

    MGS-31-Generator-generates-on-click - added color - complementer sample boxes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,7 +14,7 @@
 
 <LayoutGrid class="bg-black pt-16">
   <Cell span={3} sstyle="border: 1px solid cyan;">
-    <MGenerator></MGenerator>
+    <MGenerator />
   </Cell>
   <Cell span={3} sstyle="border: 1px solid gray;">
     <MGenerator disabled ></MGenerator>
@@ -33,45 +33,21 @@
       ></MGissBoard>
   </Cell>
 
-  <Cell span={6} style="border: 1px solid red;">
-    <div>
-      <Button onclick={() => clicked++}>
-        <Label>Default</Label>
-      </Button>
-      <Button onclick={() => clicked++} disabled>
-        <Label>Disabled</Label>
-      </Button>
-      <Button onclick={() => clicked++} ripple={false}>
-        <Label>No Ripple</Label>
-      </Button>
-      <!--
-        Note: to target this class, you need to use
-        a :global() selector. See the styles below.
-      -->
-      <Button onclick={() => clicked++} class="myClass">
-        <Label>With a Class</Label>
-      </Button>
-    </div>
-
-    <pre class="status">Clicked: {clicked}</pre>
-
-  </Cell>
-  <Cell span={6} style="border: 1px solid red;">
-    teszt
+  <Cell span={12} style="border: 1px solid purple; color: white;">
+    <ColorSamples />
   </Cell>
 </LayoutGrid>
 
 <script lang="ts">
   import "../app.css";
 
-  import Button, { Label } from '@smui/button';
   import IconButton from '@smui/icon-button';
   import LayoutGrid, { Cell } from '@smui/layout-grid';
   import TopAppBar, {Row, Section, Title} from '@smui/top-app-bar';
   import MGissBoard from "$lib/components/MGissBoard.svelte";
   import MGenerator from "$lib/components/MGenerator.svelte";
+  import ColorSamples from "$lib/components/dev/ColorSamples.svelte";
 
-  let clicked = $state(0);
 </script>
 
 <style lang="postcss">
