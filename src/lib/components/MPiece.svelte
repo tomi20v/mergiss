4f201d5e8685fb9fc1538275e07ad0a6e14dd205
tamas <tomi20v@gmail.com>
2025-01-19 00:11:43 +0100

    MGS-35-I-can-see-a-build-board - add shims to MPiece so pieces with various widths (2,3,4) have the same overall width

--- src/lib/components/MPiece.svelte
+++ src/lib/components/MPiece.svelte
@@ -1,10 +1,26 @@
-<div class="flex grow flex-col gap-px" style:max-width="{maxWidth}">
+<div class="flex flex-grow flex-col gap-0.5" style:max-width="{maxWidth}">
     {#each piece.pixelMap as eachRow}
-        <div class="flex grow flex-row gap-px">
+        <div class="flex grow flex-row gap-0.5">
+            <!-- we need these shims so things don't "jump around" when piece width changes between 2 and 4 - otherwise
+                the gapping make these cases uneven and things start jumping around
+                HOWEVER it still jumps between empty and having a piece which might happen when dragging later?
+                -->
+            {#if (w<1)}
+                <div class="flex w-0"></div>
+            {/if}
+            {#if (w<2)}
+                <div class="flex w-0"></div>
+            {/if}
+            {#if (w<3)}
+                <div class="flex w-0"></div>
+            {/if}
+            {#if (w<4)}
+                <div class="flex w-0"></div>
+            {/if}
             {#each eachRow as eachCell}
-                <div class="flex aspect-square grow"
-                     style="border-radius: 15%; {eachCell ? cellStyle : ''}"
-                ></div>
+            <div class="flex aspect-square grow"
+                 style="border-radius: 15%; {eachCell ? cellStyle : ''}">
+            </div>
             {/each}
         </div>
     {/each}
@@ -12,6 +28,7 @@
 <script lang="ts">
 
   let { piece } = $props();
+  let w = $derived(piece.pixelMap[0].length);
   let maxWidth = $derived.by(() => {
     const h = piece.pixelMap.length;
     const w = piece.pixelMap[0].length;
