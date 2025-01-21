39073b1fc0aba843cc079d11a1d9ad725a800d36
tamas <tomi20v@gmail.com>
2025-01-21 01:12:04 +0100

    MGS-35-I-can-see-a-build-board - changed the board component so it's a real component now. (the original concept was to separate game logic which would work also when this component is not shown. In other words, now I practically cannot hide the board or it will get re-initialized)

--- src/lib/components/MGissBoard.svelte
+++ src/lib/components/MGissBoard.svelte
@@ -9,7 +9,7 @@
       <button onclick={resizeAddColumn}>add</button>
     </div>
   {/if}
-  {#each board.fields as row, rowIndex}
+  {#each fields as row, rowIndex}
   <div class="flex gap-0 justify-center">
     {#each row as _, fieldIndex}
       <div class="flex ggrow xbg-stone-500"
@@ -24,47 +24,79 @@
     {/each}
   </div>
   {/each}
-  {board}
 </div>
 <script lang="ts">
 
-  import BuildBoard from "$lib/game/BuildBoard";
   import {dev} from "$app/environment";
+  import {onMount} from "svelte";
+
+  type FieldType = number;
 
   let { boardWidth, boardHeight } = $props();
 
-  // @todo this is not reactive like this, change it
-  let board: BuildBoard = $state(new BuildBoard(5,5));
-  // let board: BuildBoard = $state(new BuildBoard(10,10));
-  // let board: BuildBoard = $state(new BuildBoard(30,20));
-  // let board: BuildBoard = $state(new BuildBoard(40,20));
-  // let board: BuildBoard = $state(new BuildBoard(15,15));
-  // let board: BuildBoard = $state(new BuildBoard(42,42));
+  // @todo these shall go into some game state ("save") management
+  const sX = 6, sY = 6;
+  // const sX = 10, sY = 10;
+  // const sX = 30, sY = 20;
+  // const sX = 40, sY = 20;
+  // const sX = 15, sY = 15;
+  // const sX = 42, sY = 42;
+
+  let sizeX: number = $state(sX);
+  let sizeY: number = $state(sY);
+  let fields: FieldType[][] = $state([]);
 
   let width = $derived.by(() => {
-    const extraWidth = board.sizeX >= 5 ? 2.2 : 1.8;
-    const extraHeight = board.sizeY >= 5 ? 1 : 1.8;
-    const perWidth = Math.floor(boardWidth / (board.sizeX+extraWidth));
-    const perHeight = Math.floor(boardHeight / (board.sizeY+extraHeight));
+    // this makes sure we update the width when the table size changes
+    // const s = serial;
+    const extraWidth = sizeX >= 5 ? 2.2 : 1.8;
+    const extraHeight = sizeY >= 5 ? 1 : 1.8;
+    const perWidth = Math.floor(boardWidth / (sizeX+extraWidth));
+    const perHeight = Math.floor(boardHeight / (sizeY+extraHeight));
     const ret = Math.min(perWidth, perHeight);
     return ret + 'px';
   })
+
+  onMount(() => {
+    fields = Array.from(
+      {length: sizeX},
+      () => Array.from({length: sizeY}, emptyField)
+    );
+  })
+
+  function emptyField(): FieldType {
+    return 0;
+  }
 
   function backgroundImageOf(i: number, j: number): string {
     return `grid/${String.fromCharCode(i%6 + 65)}${j%6+1}.png`;
   }
 
   function resizeAddColumn() {
-    board.addColumn();
+    sizeX++;
+    fields.forEach(each => each.push(emptyField()));
   }
   function resizeRemoveColumn() {
-    board.removeColumn();
+    if (sizeX <= 5) {
+      return;
+    }
+    sizeX--;
+    fields.forEach(each => each.pop());
   }
   function resizeAddRow() {
-    board.addRow();
+    sizeY++;
+    const row: number[] = [];
+    for (let x=0; x<sizeX; x++) {
+      row.push(emptyField());
+    }
+    fields.push(row);
   }
   function resizeRemoveRow() {
-    board.removeRow();
+    if (sizeY <= 5) {
+      return;
+    }
+    sizeY--;
+    fields.pop();
   }
 
 </script>
