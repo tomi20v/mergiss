9b77c954f56e9d3a1e0e96764465ecdd414affd4
tamas <tomi20v@gmail.com>
2025-02-08 01:00:40 +0100

    MGS-33-I-can-rotate-pieces-when-dragging-to-build-board - better initial position for pieces

--- src/lib/game/piece/pieceCatalogue.ts
+++ src/lib/game/piece/pieceCatalogue.ts
@@ -2,8 +2,12 @@ import type {IProtoMap} from "$lib/game/piece/IProtoMap";
 
 const pieceCatalogue: IProtoMap[] = [
   [
+    [0,0,1,],
     [1,1,1,],
+  ],
+  [
     [1,0,0,],
+    [1,1,1,],
   ],
   [
     [1,1,1,1,],
@@ -23,10 +27,6 @@ const pieceCatalogue: IProtoMap[] = [
   [
     [1,1,],
     [1,1,],
-  ],
-  [
-    [1,1,1,],
-    [0,0,1,],
   ],
 ];
 
