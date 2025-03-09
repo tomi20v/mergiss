5d125b58cdd88b1038e73b486a4e2367cccc3036
tomi20v <tomi20v@gmail.com>
2025-03-09 02:57:04 +0100

    Mgs 41 deploy static on netlify (#11)
    * MGS-41-deploy-static-on-netlify - fix static building
    
    * MGS-41-deploy-static-on-netlify - gitignore, shell script
    
    * MGS-41-deploy-static-on-netlify - fix container name
    
    * MGS-41-deploy-static-on-netlify - fix build error
    
    * MGS-41-deploy-static-on-netlify - adjust base path

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,10 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
 			fallback: 'index.html'
-		})
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : '/mergiss/build'
+		}
 	}
 };
 
