ca27471075427d3d457ba03fdb1c481fba694e3f
tamas <tomi20v@gmail.com>
2025-04-13 02:14:07 +0200

    MGS-63-measure-block-movements - better PlayStore init

--- src/lib/playStore.svelte.ts
+++ src/lib/playStore.svelte.ts
@@ -6,17 +6,21 @@ class PlayStore {
   paused: boolean = $state(false);
   // generator
   availableColors: string[] = $state([]);
-  generatorTime: number = $state(0.5);
+  generatorTime: number = $state(0);
   // board
-  boardSizeX: number = $state(3);
-  boardSizeY: number = $state(3);
+  boardSizeX: number = $state(0);
+  boardSizeY: number = $state(0);
   mergeBoardCellWidth: number = $state(0);
   // scores
   score: number = $state(0);
 }
 
+const playStore: PlayStore = new PlayStore();
+
 // uiBus.on("playStart", (play: PlayConfigType) => {
-uiBus.on("playStart", () => {
+uiBus.on("playStart", initStore)
+
+function initStore() {
   playStore.paused = false;
   // generator
   playStore.generatorTime = 0.5;
@@ -25,8 +29,7 @@ uiBus.on("playStart", () => {
   playStore.boardSizeY = 3;
   // score
   playStore.score = 0;
-})
-
-const playStore: PlayStore = new PlayStore();
+}
+initStore();
 
 export default playStore;
