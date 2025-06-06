d2ff5d3a66a6cc996fd1fc0997f4f6aac5d219b6
tamas <tomi20v@gmail.com>
2025-06-06 02:15:55 +0200

    MGS-138-astro-1 - bump, fix text size

--- src/lib/components/scores/MBonusCard.svelte
+++ src/lib/components/scores/MBonusCard.svelte
@@ -1,9 +1,9 @@
-<div class="panel-border flex bonus-container">
+<div class="panel-border flex bonus-container"
+     title="Bonus multiplier! All scores are multiplied" >
     <div class="bonus-value">
         <span class="multiplier-x golden-dark-text">×</span>
         <span class="multiplier-number golden-text golden-text-shadow">{showValue}</span>
     </div>
-    <div class="bonus-glow"></div>
 </div>
 
 <script lang="ts">
@@ -15,7 +15,6 @@
 <style>
   .bonus-container {
     position: relative;
-    min-height: 5rem;
     justify-content: center;
     align-items: center;
     overflow: hidden;
@@ -27,35 +26,19 @@
     justify-content: center;
     font-family: 'Orbitron', sans-serif;
     font-weight: bold;
-    padding: 0.5rem 1rem;
+    padding: 0.5vw 1vw;
     z-index: 2;
-    /*text-shadow: 0 0 15px #ffb700, 0 0 25px #ff9500;*/
-    /*animation: pulse 2.5s infinite ease-in-out;*/
   }
 
   .multiplier-x {
-    font-size: 1.5rem;
-    /*color: #ffa726;*/
-    margin-right: 0.25rem;
+    font-size: 1.5vw;
+    margin-right: 0.25vw;
     opacity: 0.8;
   }
 
   .multiplier-number {
-    font-size: 2.5rem;
-    /*color: #ffd700;*/
+    font-size: 2.5vw;
     letter-spacing: 0.05em;
-  }
-
-  .bonus-glow {
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    /*background: radial-gradient(circle at center, rgba(255, 215, 0, 0.3) 0%, transparent 70%);*/
-    opacity: 0.7;
-    z-index: 1;
-    /*animation: glow 3s infinite alternate ease-in-out;*/
   }
 
   @keyframes pulse {
